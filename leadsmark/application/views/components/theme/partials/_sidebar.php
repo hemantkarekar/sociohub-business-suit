5826ec28ebb3628c2e8c1a815fc7e54bcc6524bd
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-04-15 20:32:44 +0530

    Dashboards

---
+++ leadsmark/application/views/components/theme/partials/_sidebar.php
@@ -0,0 +1,129 @@
+<nav class="sidebar">
+	<div class="sidebar-header">
+		<a href="#" class="sidebar-brand">
+			Noble<span>UI</span>
+		</a>
+		<div class="sidebar-toggler not-active">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</div>
+	<div class="sidebar-body">
+		<ul class="nav">
+			<li class="nav-item nav-category">Leads Analytics</li>
+			<li class="nav-item">
+				<a href="dashboard.html" class="nav-link">
+					<i class="link-icon" data-feather="box"></i>
+					<span class="link-title">Dashboard</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href="dashboard.html" class="nav-link">
+					<i class="link-icon" data-feather="database"></i>
+					<span class="link-title">All Leads</span>
+				</a>
+			</li>
+
+			<li class="nav-item nav-category">Restaurant Management</li>
+			<li class="nav-item">
+				<a href="dashboard.html" class="nav-link">
+					<i class="link-icon" data-feather="box"></i>
+					<span class="link-title">Dashboard</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-bs-toggle="collapse" href="#emails" role="button" aria-expanded="false" aria-controls="emails">
+					<i class="link-icon" data-feather="coffee"></i>
+					<span class="link-title">Menu Management</span>
+					<i class="link-arrow" data-feather="chevron-down"></i>
+				</a>
+				<div class="collapse" id="emails">
+					<ul class="nav sub-menu">
+						<li class="nav-item">
+							<a href="pages/email/inbox.html" class="nav-link">Categories</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/email/read.html" class="nav-link">Menu Items</a>
+						</li>
+					</ul>
+				</div>
+			</li>
+
+			<li class="nav-item nav-category">Content Management</li>
+			<li class="nav-item">
+				<a href="dashboard.html" class="nav-link">
+					<i class="link-icon" data-feather="box"></i>
+					<span class="link-title">Dashboard</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-bs-toggle="collapse" href="#posts" role="button" aria-expanded="false" aria-controls="posts">
+					<i class="link-icon" data-feather="book"></i>
+					<span class="link-title">Blogs Posts</span>
+					<i class="link-arrow" data-feather="chevron-down"></i>
+				</a>
+				<div class="collapse" id="posts">
+					<ul class="nav sub-menu">
+						<li class="nav-item">
+							<a href="pages/email/inbox.html" class="nav-link">Categories</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/email/read.html" class="nav-link">All Posts</a>
+						</li>
+					</ul>
+				</div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-bs-toggle="collapse" href="#articles" role="button" aria-expanded="false" aria-controls="articles">
+					<i class="link-icon" data-feather="book"></i>
+					<span class="link-title">News Articles</span>
+					<i class="link-arrow" data-feather="chevron-down"></i>
+				</a>
+				<div class="collapse" id="articles">
+					<ul class="nav sub-menu">
+						<li class="nav-item">
+							<a href="pages/email/inbox.html" class="nav-link">Categories</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/email/read.html" class="nav-link">All Articles</a>
+						</li>
+					</ul>
+				</div>
+			</li>
+
+			<li class="nav-item nav-category">Campaigns Management</li>
+			<li class="nav-item">
+				<a href="dashboard.html" class="nav-link">
+					<i class="link-icon" data-feather="box"></i>
+					<span class="link-title">Dashboard</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-bs-toggle="collapse" href="#campaigns" role="button" aria-expanded="false" aria-controls="campaigns">
+					<i class="link-icon" data-feather="pie-chart"></i>
+					<span class="link-title">Campaigns</span>
+					<i class="link-arrow" data-feather="chevron-down"></i>
+				</a>
+				<div class="collapse" id="campaigns">
+					<ul class="nav sub-menu">
+						<li class="nav-item">
+							<a href="pages/email/inbox.html" class="nav-link">All Campaigns</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/email/read.html" class="nav-link">Analtics</a>
+						</li>
+					</ul>
+				</div>
+			</li>
+
+			<li class="nav-item nav-category">Docs</li>
+			<li class="nav-item">
+				<a href="https://www.nobleui.com/html/documentation/docs.html" target="_blank" class="nav-link">
+					<i class="link-icon" data-feather="hash"></i>
+					<span class="link-title">Documentation</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+</nav>
